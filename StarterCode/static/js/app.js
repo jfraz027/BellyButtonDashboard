aec8696b3a905ba51d712b0630f8f9b7f0f88f48
jfraz027 <jfrazier027@gmail.com>
2022-06-25 09:56:13 -0400

    demo table change

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,16 +13,19 @@ function init() {
     });
 
     let firstsample = sampleNames[0];
-    //  buildcharts(firstsample);
+    //buildcharts(firstsample);
     builddemotable(firstsample);
   });
-  // Function to change options
-  function changeoption(newsample) {
-    // buildcharts(newsample);
-    builddemotable(newsample);
-  }
+  
 }
 init();
+
+// Function to change options
+function optionChanged(newsample) {
+    //buildcharts(newsample);
+    builddemotable(newsample);
+}
+
 // Function to pull the Metadata into Demographic Table
 function builddemotable(sample) {
   d3.json(
@@ -38,13 +41,49 @@ function builddemotable(sample) {
       panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
 
+
+
+
+
+
+
     // buildGuage(result.wfreq)
   });
 }
 
 // Function to build Charts
 // function buildcharts(sample){
-//     d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data)=> {
+//      d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+// ).then((data)=> {
+//     let samples= data.samples;
+//     let resultsarray= samples.filter(sampleobject => 
+//     sampleobject.id == sample);
+//     let result= resultsarray[0]
 
-// });
-// };
+//     let ids = result.otu_ids;
+//     let labels = result.otu_labels;
+//     let values = result.sample_values;
+
+//     var LayoutBubble = {
+//         margin: { t: 0 },
+//         xaxis: { title: "OTU ID" },
+//         hovermode: "closest",
+//         };
+    
+//         var DataBubble = [ 
+//         {
+//           x: ids,
+//           y: values,
+//           text: labels,
+//           mode: "markers",
+//           marker: {
+//             color: ids,
+//             size: values,
+//             }
+//         }
+//       ];
+    
+//       Plotly.newPlot("bubble", DataBubble, LayoutBubble);
+
+//  });
+//  };
